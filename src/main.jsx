0bca632f34d55521cc617b6bdfc633bbfde78193
urvashi <urvashikshatriya9@gmail.com>
2025-07-18 22:50:20 +0530

    Project should be deployed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../src/css/index.css'
+import './css/index.css'
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/Quiz_App">
   <StrictMode>
     <App />
   </StrictMode>,
